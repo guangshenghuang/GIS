bc328e737e0e7ea114a08b5f85829900e03d939b
guangshenghuang <konsonhuang@outlook.com>
2022-11-01 21:45:34 +0000

    Nov 11

--- wk4_homework.R
+++ wk4_homework.R
@@ -1,8 +1,11 @@
 library(sf)
 library(tidyverse)
 library(janitor)
-world <- st_read('G:/UCL/GIS/wk4/World_Countries__Generalized_.shp') %>% clean_names()
-gender <- read_csv('G:/UCL/GIS/wk4/HDR21-22_Composite_indices_complete_time_series.csv') %>%
+library(here)
+
+world <- st_read(here('wk4_data/World_Countries__Generalized_.shp'))%>% 
+  clean_names()
+gender <- read_csv(here('wk4_data/HDR21-22_Composite_indices_complete_time_series.csv')) %>%
   clean_names() 
 gender_gii <- gender %>% select(country,gii_2019,gii_2010)
 
@@ -11,6 +14,9 @@ world_omit <- world %>% na.omit()
 gender_omit <- gender_gii %>% na.omit()
 
 gender_difference <- gender_omit %>% 
-  mutate()
+  mutate(difference=gii_2019-gii_2010)
 
-print('')
+gender_spatial <- world_omit %>% 
+  left_join(gender_difference,by=c("country"="country"))
+
+
